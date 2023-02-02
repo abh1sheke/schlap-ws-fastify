867e46a67a185ea9cb2b562435f83a5f1ea94361
shekmeister <95183266+shekmeister@users.noreply.github.com>
2023-02-02 18:48:48 +0530

    init socket io

--- index.ts
+++ index.ts
@@ -10,6 +10,16 @@ server.register(fastifyIO, {
   cors: { origin: process.env?.["ALLOWED_URL"] },
 });
 
+server.ready().then(() => {
+  server.io.use((socket, next) => {
+    socket.userId = socket.handshake.auth["userId"];
+    next();
+  });
+  server.io.on("connection", (socket) => {
+    socket.userId && socket.join(socket.userId);
+  });
+});
+
 server.listen(
   { port: parseInt(process.env?.["WS_PORT"] as string) },
   (err, address) => {
