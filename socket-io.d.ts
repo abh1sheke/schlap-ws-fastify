8680eff491e7b7d4fb3480d1270c864f43115aec
shekmeister <95183266+shekmeister@users.noreply.github.com>
2023-02-02 18:48:24 +0530

    update socket.io declare module

--- socket-io.d.ts
+++ socket-io.d.ts
@@ -1,11 +1,12 @@
 import { Socket as DefaultSocket } from "socket.io";
 
 declare module "socket.io" {
-  interface ISocket extends DefaultSocket {
+  interface Socket {
+    userId: string | undefined;
     handshake: {
       auth: {
-        id: string | undefined;
-      };
+        userId: string | undefined;
+      } & DefaultSocket["auth"];
     } & DefaultSocket["handshake"];
   }
 }
